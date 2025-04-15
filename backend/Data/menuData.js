2a82934ef4e14c1ba2d05c00cd47c6cd91a789d9
Macfatty <ayumoh96@outlook.com>
2025-04-15 22:11:12 +0200

    meny 1-21 klar

--- backend/Data/menuData.js
+++ backend/Data/menuData.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const menyPath = path.join(__dirname, "campino_meny_slutgiltig.json");
+const menyPath = path.join(__dirname, "meny.json");
 // const menyPath = path.join(__dirname, "menuData.json");
 const menyData = JSON.parse(fs.readFileSync(menyPath, "utf8"));
 
